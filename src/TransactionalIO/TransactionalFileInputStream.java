901d2e18aa34cbb9f71da3bd20b8e827a4e8a29b
Ashok Chandrasekar <ashoksekar07@gmail.com>
2013-09-05 00:43:25 -0400

    transactional io basic

--- src/TransactionalIO/TransactionalFileInputStream.java
+++ src/TransactionalIO/TransactionalFileInputStream.java
@@ -1,5 +1,39 @@
 package TransactionalIO;
 
-public class TransactionalFileInputStream {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+public class TransactionalFileInputStream extends FileInputStream implements java.io.Serializable {
+	protected int fileOffset;
+	protected int fileSize;
+	
+	public TransactionalFileInputStream(File file) throws FileNotFoundException {
+		super(file);
+		this.fileOffset = 0;
+		try {
+			this.fileSize = this.available();
+		}
+		catch(IOException e) {
+			this.fileSize = -1;
+		}
+	}
+	
+	public String readLine() throws IOException {
+		int i, j=0;
+		char[] array = new char[2400];
+		
+		this.skip(this.fileOffset);
+		i = this.read();
+		this.fileOffset++;
+		
+		while(i != -1 && i != 46) {
+			array[j++] = (char)i;
+			i = this.read();
+			this.fileOffset++;
+		}
+		
+		return new String(array);
+	}
 }
--- src/TransactionalIO/TransactionalFileOutputStream.java
+++ src/TransactionalIO/TransactionalFileOutputStream.java
@@ -1,5 +1,13 @@
 package TransactionalIO;
 
-public class TransactionalFileOutputStream {
+import java.io.*;
 
+public class TransactionalFileOutputStream extends FileOutputStream implements Serializable {
+	protected int fileOffset;
+	
+	public TransactionalFileOutputStream(File file) throws FileNotFoundException {
+		super(file);
+		this.fileOffset = 0;
+	}
 }
+
